4ea9b4acce1005b208d115eab3eb362bc434c202
unknown <arvydas@balionis.com>
2020-07-25 16:52:58 +0100

    JIRA-0000: clone of kotlin0 to kotlin1

---
+++ balionis-kotlin1/README.md
@@ -0,0 +1,17 @@
+# Why?
+It's a kotlin prototype project to remind me how to use guice injection with kotlin. 
+The baseline is balionis-kotlin0.
+
+# Build
+```
+gradle clean test jar
+```
+
+# Run
+```
+gradle demo -Dmyvariable=myvalue -Dexec.args="myarg1 myarg2"
+```
+
+# Test
+
+TODO
---
+++ balionis-kotlin1/src/test/kotlin/com/balionis/kotlin1/AppTest.kt
@@ -0,0 +1,24 @@
+package com.balionis.kotlin1
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class AppTest {
+    @Test
+    fun testMe() {
+        val res = App.echo(listOf("test"))
+        assertEquals("echo:test", res)
+    }
+
+    @Test
+    fun testMe2() {
+        val res = App.echo(listOf("test1", "test2"))
+        assertEquals("echo:test1", res)
+    }
+
+    @Test
+    fun testDefault() {
+        val res = App.echo(listOf())
+        assertEquals("echo:default", res)
+    }
+}
