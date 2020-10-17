30160744817527e86b96a77baf51cf01691aa123
unknown <arvydas@balionis.com>
2020-10-17 20:06:40 +0200

    JIRA-0000: clone kotlin4

---
+++ balionis-kotlin5/README.md
@@ -0,0 +1,13 @@
+# Why?
+It's a activiti prototype using gradle, kotlin as baseline.
+
+# Build
+```
+gradle clean test build
+```
+
+# Run
+```
+gradle run --args="myarg1 myarg2"
+```
+
---
+++ balionis-kotlin5/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    application
+    kotlin("jvm") version "1.4.10"
+    kotlin("kapt") version "1.4.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+apply {
+    plugin("kotlin")
+    plugin("kotlin-kapt")
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    implementation("com.squareup.moshi:moshi:1.9.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.2")
+
+    implementation("io.github.microutils:kotlin-logging:1.11.5")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+
+    testImplementation("junit:junit:4.11")
+    testImplementation(kotlin("test-junit"))
+
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.9.3")
+}
+
+application {
+    mainClassName = "com.balionis.kotlin5.AppKt"
+}
