3b8292c6bfbe3cdcc172b66d968c2661e7eba830
unknown <arvydas@balionis.com>
2020-10-18 22:06:06 +0200

    JIRA-0000: added activiti init

--- balionis-kotlin5/src/main/kotlin/com/balionis/kotlin5/App.kt
+++ balionis-kotlin5/src/main/kotlin/com/balionis/kotlin5/App.kt
@@ -1,26 +1,48 @@
 package com.balionis.kotlin5
 
 import mu.KotlinLogging
+import org.activiti.engine.ProcessEngine
+import org.activiti.engine.ProcessEngineConfiguration
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration
+import org.activiti.engine.repository.Deployment
 
 private val logger = KotlinLogging.logger {}
 
-object App {
+class App {
 
-    fun echo(req: String): String {
-        logger.debug { "echo: req=${req}" }
+    private lateinit var processEngine: ProcessEngine
 
-        return req
+    fun init(args: List<String>) {
+        logger.debug { "init: args=${args.joinToString()}" }
+
+        val cfg = StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:h2:mem:activiti;DB_CLOSE_DELAY=1000")
+                .setJdbcUsername("sa")
+                .setJdbcPassword("")
+                .setJdbcDriver("org.h2.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE)
+        processEngine = cfg.buildProcessEngine()
+        logger.debug { "init: pName=${processEngine.name}, version=${ProcessEngine.VERSION}" }
+    }
+
+    fun deploy(flow: String) {
+        val repositoryService = processEngine.repositoryService
+        val deployment: Deployment = repositoryService.createDeployment()
+                .addClasspathResource(flow).deploy()
+        val processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId()).singleResult()
+        logger.debug { "deploy: name=${processDefinition.name}, id=${processDefinition.id}" }
     }
 }
 
 fun main(args: Array<String>) {
 
-    val req = args.joinToString()
+    val app = App()
 
-    logger.debug { "main: req=${req}" }
+    app.init(args.asList())
 
-    val res = App.echo(req)
+    app.deploy("sample2.bpmn20.xml")
 
-    logger.debug { "main: res=${res}" }
+    logger.debug { "main: done" }
 
 }
--- balionis-kotlin5/src/test/kotlin/com/balionis/kotlin5/AppTest.kt
+++ balionis-kotlin5/src/test/kotlin/com/balionis/kotlin5/AppTest.kt
@@ -1,12 +1,23 @@
 package com.balionis.kotlin5
 
 import org.junit.Test
-import kotlin.test.assertEquals
+import org.junit.Before
+import java.util.*
+import kotlin.test.assertTrue
 
 class AppTest {
+
+    private lateinit var app: App
+
+    @Before
+    fun setUp() {
+       app = App()
+    }
+
     @Test
     fun testMe() {
-        val actual = App.echo("arg1")
-        assertEquals("arg1", actual)
+        val args = arrayOf("args1")
+        app.init(args.asList())
+        assertTrue(true)
     }
 }
