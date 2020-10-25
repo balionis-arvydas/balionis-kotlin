f6a49390004cc2cd6e2a151ce570f9246eb496f6
arvydas balionis <arvydas@balionis.com>
2020-10-25 12:07:09 +0100

    JIRA-0000: add kafka

--- balionis-kotlin7/README.md
+++ balionis-kotlin7/README.md
@@ -9,3 +9,8 @@ $ ./gradlew clean build docker
 
 ## Test
 
+```
+$ docker exec -it 22e52d6ffec4 bash
+$ ps auxww | grep zookeeper
+$ ps auxww | grep kotlin7 
+```
--- balionis-kotlin7/build.gradle.kts
+++ balionis-kotlin7/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging:1.11.5")
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
+
     testImplementation("junit:junit:4.11")
     testImplementation(kotlin("test-junit"))
 
