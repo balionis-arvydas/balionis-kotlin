232322fcef834806c9f7cd369e45c0218313ca78
arvydas balionis <arvydas@balionis.com>
2020-11-07 18:30:58 +0100

    JIRA-0000: added logback, added docker

--- balionis-kotlin9/build.gradle.kts
+++ balionis-kotlin9/build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.5.RELEASE"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
+	id("org.springframework.boot") version "2.3.5.RELEASE"
+	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+	id("com.palantir.docker") version "0.25.0"
+	id("com.palantir.docker-compose") version "0.25.0"
 }
 
 group = "com.balionis"
@@ -20,6 +22,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("io.github.microutils:kotlin-logging:1.11.5")
+	implementation("ch.qos.logback:logback-classic:1.2.3")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
--- balionis-kotlin9/src/main/kotlin/com/balionis/kotlin9/MyApp.kt
+++ balionis-kotlin9/src/main/kotlin/com/balionis/kotlin9/MyApp.kt
@@ -4,11 +4,19 @@ import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 @SpringBootApplication
 class MyApp
 
 fun main(args: Array<String>) {
+	logger.debug { "main: args=${args.joinToString()}" }
+
 	runApplication<MyApp>(*args) {
 		setBannerMode(Banner.Mode.OFF)
 	}
+
+	logger.debug { "main:-" }
 }
